1ad78cb26dbf6a3344fe5d0136590b3cf0d2d01a
destripador2000 <davidhumbertop503@gmail.com>
2026-06-15 08:49:42 -0600

    Refactor: Refactorización de código del modelo User, agregando relación con las demás tablas y función __repr__

--- backend/app/models/md_User.py
+++ backend/app/models/md_User.py
@@ -1,26 +1,41 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from enum import Enum
+from typing import TYPE_CHECKING
 
-from sqlalchemy import String, DateTime, ForeignKey
+from sqlalchemy import String, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.models.base import Base
 
+# Asegurandonos que no haya una referencia circular
+if TYPE_CHECKING:
+    from app.models.md_ProfileClient import ProfileClient
+    from app.models.md_ProfileLawyer import ProfileLawyer
 
-# Creación de enums
+
+# Creando Enum de los roles de usuario
 class UserRole(str, Enum):
     LAWYER = "abogado"
     CLIENT = "cliente"
 
 
-# Creamos clase de modelo users para tabla en la bd 
-class Users(Base):
-    __tablename__ =  "users" # Nombre de la tabla    
+# Creando clase User que servirá como ORM 
+class User(Base):
+    __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True) # Id de la tabla
-    email: Mapped[str] = mapped_column(String(255), nullable= False) # Email del usuario
-    password_hash: Mapped[str] = mapped_column(String(50), nullable=False) # Contraseña
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False) # Fecha de creación del usuario
-    is_active: Mapped[bool] = mapped_column(default=True, nullable=False) # Usuario activo/inactivo
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
     role: Mapped[str] = mapped_column(String(20), nullable=False, default=UserRole.CLIENT.value)
 
-    # Aquí irán las relaciones
+    # Instanceando relación entre las demás tablas (profile_lawyer y profile_client)
+    profile_lawyer: Mapped["ProfileLawyer"] = relationship(back_populates="user", uselist=False, cascade="all, delete-orphan")
+    profile_client: Mapped["ProfileClient"] = relationship(back_populates="user", uselist=False, cascade="all, delete-orphan")
+
+
+    # Integrando función __repr__ que sirve para manejo de errores
+    def __repr__(self) -> str:
+        return f"User(id={self.id}, email={self.email}, role={self.role})"
