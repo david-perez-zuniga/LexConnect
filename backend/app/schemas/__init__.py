dcf191162967f36368324432161fa2d2d457e3d6
destripador2000 <davidhumbertop503@gmail.com>
2026-06-16 08:40:28 -0600

    Feat: Instanceando configuracion centralizada para todos los esquemas, esto permite que los imports no sean largos y ensucien el código

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,6 +1,45 @@
 from pydantic import BaseModel, ConfigDict
-from datetime import datetime
 
-
+# Función de configuración centralizada para todos los esquemas como lo dictamina pydantic v2
 class BaseSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True, str_strip_whitespace=True)
+
+# Configuración para el esquema user
+from .user_schema import (
+    UserBase,
+    UserCreate,
+    UserRead,
+    UserUpdate,
+)
+
+# Configuración para el esquema profile_lawyer
+from .profile_lawyer_schema import (
+    ProfileLawyerBase,
+    ProfileLawyerCreate,
+    ProfileLawyerRead,
+    ProfileLawyerUpdate,
+)
+
+# Configuración para el esquema profile_client
+from .profile_client_schema import (
+    ProfileClientBase,
+    ProfileClientCreate,
+    ProfileClientRead,
+    ProfileClientUpdate,
+)
+
+__all__ = [
+    "BaseSchema",
+    "UserBase",
+    "UserCreate",
+    "UserRead",
+    "UserUpdate",
+    "ProfileLawyerBase",
+    "ProfileLawyerCreate",
+    "ProfileLawyerRead",
+    "ProfileLawyerUpdate",
+    "ProfileClientBase",
+    "ProfileClientCreate",
+    "ProfileClientRead",
+    "ProfileClientUpdate",
+]
