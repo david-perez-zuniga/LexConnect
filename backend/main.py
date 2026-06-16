2f8550164d90b4cae777d516852865c822e5d20b
destripador2000 <davidhumbertop503@gmail.com>
2026-06-16 08:54:12 -0600

    Feat: Agregando la ruta del v1 de los endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.api.v1 import api_router
 
-app = FastAPI()
+app = FastAPI(title="LexConnect API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,3 +12,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(api_router, prefix="/api/v1")
