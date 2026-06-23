31392e32e24a52d182cc07c27c3b67c4393a56ef
destripador2000 <davidhumbertop503@gmail.com>
2026-06-23 07:09:25 -0600

    Feat: Creación de contrato para el login

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,11 +1,19 @@
 export type UserRole = 'cliente' | 'abogado'
 
+// Contrato para login
+export interface UserLogin {
+  email: string
+  password: string
+}
+
+// Contrato para Registro
 export interface RegisterPayload {
   email: string
   password: string
   role: UserRole
 }
 
+// Contrato para mostrar/response
 export interface UserResponse {
   id: number
   email: string
@@ -15,6 +23,7 @@ export interface UserResponse {
   updated_at: string
 }
 
+// Contrato para mostrar error
 export interface ApiError {
   detail: string
 }
