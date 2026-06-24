ac59013811d2713c493c6eb376d30110f4fa8d20
destripador2000 <davidhumbertop503@gmail.com>
2026-06-24 10:21:50 -0600

    Feat: Creación de ENDPOINT GET para obtener el usuario logueado

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -1,15 +1,20 @@
 from datetime import timedelta
+from genericpath import _AllowMissingType
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from sqlalchemy.sql.functions import current_user
+from sqlalchemy.sql.type_api import UserDefinedType
 
 from app.core.config import settings
 from app.core.database import get_db
 from app.core.security import create_access_token, verify_password
 from app.models.md_User import User
-from app.schemas.user_schema import LoginRequest
+from app.schemas.user_schema import LoginRequest, UserRead
 from app.core.logger import logger
+from app.api.deps import get_current_user
 
 
 router = APIRouter(tags=["auth"])
@@ -52,3 +57,27 @@ async def login(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error inesperado al iniciar sesión: {str(e)}",
         )
+
+# API para obtener al usuario conectado
+@router.get("/me", response_model=UserRead)
+async def read_current_user_me(
+    current_user: User = Depends(get_current_user),
+    conex: AsyncSession = Depends(get_db)):
+
+    try:
+        stmt = select(User).options(
+        selectinload(User.profile_client)).where(
+        User.id == current_user.id)
+
+        result =  await conex.execute(stmt)
+        user = result.scalar_one
+
+    except HTTPException:
+        raise
+    except Exception as e:
+        logger.exception("Error inesperado en el perfil")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Eror inesperado al obtener usuario: {str(e)}", 
+        )
+    return user
